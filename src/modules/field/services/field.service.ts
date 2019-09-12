210bc3a1d6c554bb59f52ec9c1ad2fcb990c9094
Walter V. Faustine <waltervfaustine@gmail.com>
2019-09-12 13:23:13 +0300

    refactor: 💡 updated the entity uuid generator

---
+++ src/modules/field/services/field.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { Field } from '../entities/field.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BaseService } from 'src/core/services/base.service';
+
+@Injectable()
+export class FieldService extends BaseService<Field> {
+    constructor(
+        @InjectRepository(Field)
+        repository: Repository<Field>,
+    ) {
+        super(repository, Field);
+    }
+}
