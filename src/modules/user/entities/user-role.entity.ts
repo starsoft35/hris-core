5ee96df3c88d901b717c1f90246be044db34e193
Walter V. Faustine <waltervfaustine@gmail.com>
2019-09-25 17:10:54 +0300

    refactor: 💡 modified user role entity

--- src/modules/user/entities/user-role.entity.ts
+++ src/modules/user/entities/user-role.entity.ts
@@ -1,20 +1,20 @@
-import { Column, Entity, ManyToMany, JoinTable, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  ManyToMany,
+  JoinTable,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { User } from './user.entity';
 import { UserGroup } from './user-group.entity';
 import { UserIdentification } from './user-identification';
 
 @Entity('userrole', { schema: 'public' })
 export class UserRole extends UserIdentification {
-  @PrimaryGeneratedColumn({
-    name: 'userroleid',
-  })
+  @PrimaryGeneratedColumn()
   id: number;
 
-  @Column('character varying', {
-    nullable: false,
-    length: 64,
-    name: 'name',
-  })
+  @Column({ type: 'varchar', nullable: false, length: 64 })
   name: string;
 
   @ManyToMany(type => User, user => user.userRoles)
