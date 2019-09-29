b506739dbcfef2d679735fa17880b38720b84b34
Vincent P. Minde <vincentminde@gmail.com>
2019-09-29 12:52:03 +0300

    feat(User Role API):

--- src/modules/user/controllers/user-role.controller.ts
+++ src/modules/user/controllers/user-role.controller.ts
@@ -4,11 +4,12 @@ import { User } from 'src/modules/user/entities/user.entity';
 import { AuthGuard } from '@nestjs/passport';
 import { BaseController } from 'src/core/controllers/base.contoller';
 import { UserRole } from '../entities/user-role.entity';
+import { UserRoleService } from '../services/user-role.service';
 
 @Controller('api/' + UserRole.plural)
 //@UseGuards(AuthGuard())
 export class UserRolesController extends BaseController<UserRole> {
-    constructor(private readonly userService: UserService) {
-        super(userService, UserRole);
+    constructor(private readonly userRoleService: UserRoleService) {
+        super(userRoleService, UserRole);
     }
 }
---
+++ src/modules/user/services/user-role.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { BaseService } from '../../../core/services/base.service';
+import { User } from '../entities/user.entity';
+import { UserRole } from '../entities/user-role.entity';
+
+@Injectable()
+export class UserRoleService extends BaseService<UserRole> {
+  constructor(
+    @InjectRepository(UserRole)
+    private readonly userRoleRepository: Repository<UserRole>,
+  ) {
+    super(userRoleRepository, User);
+  }
+}
