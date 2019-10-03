14d142c733c5d38f2e3ffc6b3bc5d54ed22bb57b
Bennett <bennybenester@gmail.com>
2019-10-03 10:05:00 +0300

    fix: remove duplicate transpiled .js files
    I removed duplicate files that were transpiled to .js

--- src/core/constants/statuscode.constant.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ENTITY_NOT_FOUND = 404;
-//# sourceMappingURL=statuscode.constant.js.map
--- src/core/utilities/string-utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getSingularity(word) {
-    return word ? word.slice(0, -1) : '';
-}
-exports.getSingularity = getSingularity;
-//# sourceMappingURL=string-utils.js.map
