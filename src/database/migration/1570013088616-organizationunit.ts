db869e879fbfad26addc4ab13d2be6c3e0fe2e2e
Bennett <bennybenester@gmail.com>
2019-11-15 07:50:46 +0300

    fix(migration): correct naming typos for columns

--- src/database/migration/1570013088616-organizationunit.ts
+++ src/database/migration/1570013088616-organizationunit.ts
@@ -18,16 +18,15 @@ export class organizationunit1570013088616 implements MigrationInterface {
         await queryRunner.query('ALTER TABLE "organisationunit" ADD COLUMN IF NOT EXISTS "featuretype" text');
         await queryRunner.query('ALTER TABLE "organisationunit" ADD COLUMN IF NOT EXISTS "address" text');
         await queryRunner.query('ALTER TABLE "organisationunit" ADD COLUMN IF NOT EXISTS "email" text');
-        await queryRunner.query('ALTER TABLE "organisationunit" ADD COLUMN IF NOT EXISTS "phoneinteger" text');
         await queryRunner.query('ALTER TABLE "organisationunit" ADD COLUMN IF NOT EXISTS "contactperson" text');
         await queryRunner.query('ALTER TABLE "organisationunit" ADD COLUMN IF NOT EXISTS "description" text');
         await queryRunner.query('ALTER TABLE "organisationunit" ADD COLUMN IF NOT EXISTS "code" character varying(25)');
-        await queryRunner.query('ALTER TABLE "organisationunit" ADD COLUMN IF NOT EXISTS publicaccess character varying(8)');
+        await queryRunner.query('ALTER TABLE "organisationunit" ADD COLUMN IF NOT EXISTS "publicaccess" character varying(8)');
         await queryRunner.query('ALTER TABLE "organisationunit" ADD COLUMN IF NOT EXISTS "externalaccess" boolean');
         await queryRunner.query('ALTER TABLE "organisationunit" ADD COLUMN IF NOT EXISTS "phonenumber" text');
         await queryRunner.query('ALTER TABLE "organisationunit" RENAME COLUMN "datecreated" TO "created"');
         await queryRunner.query('ALTER TABLE "organisationunit" ADD COLUMN IF NOT EXISTS "lastupdated" timestamp without time zone');
-        await queryRunner.query('ALTER TABLE "organisationunit" ADD COLUMN IF NOT EXISTS "name" character varying(256)');  
+        await queryRunner.query('ALTER TABLE "organisationunit" RENAME COLUMN "longname" TO "name');  
         await queryRunner.query('ALTER TABLE "organisationunit" ADD COLUMN IF NOT EXISTS "lastupdatedby" character varying');
    
         await queryRunner.query('ALTER TABLE "hris_organisationunitlevel" RENAME TO "organisationunitlevel"');
@@ -38,7 +37,7 @@ export class organizationunit1570013088616 implements MigrationInterface {
         await queryRunner.query('ALTER TABLE "organisationunitlevel" ADD COLUMN IF NOT EXISTS "level" integer');
         await queryRunner.query('ALTER TABLE "organisationunitlevel" ADD COLUMN IF NOT EXISTS "description" text');
         await queryRunner.query('ALTER TABLE "organisationunitlevel" ADD COLUMN IF NOT EXISTS "name" character varying(256)');
-        await queryRunner.query('ALTER TABLE "organisationunitlevel" ADD COLUMN IF NOT EXISTS publicaccess character varying(8)');
+        await queryRunner.query('ALTER TABLE "organisationunitlevel" ADD COLUMN IF NOT EXISTS "publicaccess" character varying(8)');
         await queryRunner.query('ALTER TABLE "organisationunitlevel" ADD COLUMN IF NOT EXISTS "externalaccess" boolean');
         await queryRunner.query('ALTER TABLE "organisationunitlevel" ADD COLUMN IF NOT EXISTS "code" character varying(25)');
         await queryRunner.query('ALTER TABLE "organisationunitlevel" ADD COLUMN IF NOT EXISTS "lastupdatedby" character varying');
@@ -85,7 +84,7 @@ export class organizationunit1570013088616 implements MigrationInterface {
             await queryRunner.query('ALTER TABLE "organisationunitcompleteness" ADD COLUMN IF NOT EXISTS "lastupdated" timestamp without time zone');
             await queryRunner.query('ALTER TABLE "organisationunitcompleteness" ADD COLUMN IF NOT EXISTS "expectation" integer');
             await queryRunner.query('ALTER TABLE "organisationunitcompleteness" ADD COLUMN IF NOT EXISTS "lastupdatedby" character varying');
-            await queryRunner.query('ALTER TABLE "organisationunitcompleteness" ADD COLUMN IF NOT EXISTS publicaccess character varying(8)');
+            await queryRunner.query('ALTER TABLE "organisationunitcompleteness" ADD COLUMN IF NOT EXISTS "publicaccess" character varying(8)');
             await queryRunner.query('ALTER TABLE "organisationunitcompleteness" ADD COLUMN IF NOT EXISTS "externalaccess" text');
             await queryRunner.query('ALTER TABLE "organisationunitcompleteness" ADD COLUMN IF NOT EXISTS "code" character varying(25)');
             await queryRunner.query('ALTER TABLE "organisationunitcompleteness" ADD COLUMN IF NOT EXISTS "name" character varying(256)');
