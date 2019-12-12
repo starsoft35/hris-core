d361db05d59ae2ec7403583538c68c5e1642ffb8
Walter V. Faustine <waltervfaustine@gmail.com>
2019-12-12 11:10:59 +0300

    feat: 🎸 added core controller for maintenance module
    This commit will add core controller for maintenance module

---
+++ src/core/maintenance/controllers/base.contoller.ts
@@ -0,0 +1,236 @@
+import {
+  Body,
+  Get,
+  Post,
+  Put,
+  Param,
+  Delete,
+  Query,
+  UseGuards,
+  Res,
+  Req,
+} from '@nestjs/common';
+import { SessionGuard } from 'src/modules/system/user/guards/session.guard';
+import { Request, Response, response } from 'express';
+import * as _ from 'lodash';
+import { HRISBaseEntity } from 'src/core/entities/base-entity';
+import { BaseService } from 'src/core/services/base.service';
+import { ApiResult, Pager } from 'src/core/interfaces';
+import { getPagerDetails } from 'src/core/utilities';
+import { convertUidsToIds } from 'src/core/utilities/convertIds';
+import {
+  getSuccessResponse,
+  genericFailureResponse,
+  entityExistResponse,
+  postSuccessResponse,
+  deleteSuccessResponse,
+} from 'src/core/helpers/response.helper';
+import { IDTOUIDObjectPropsResolver } from 'src/core/resolvers/id-to-uid-object-prop.resolver';
+import { DeleteResponse } from 'src/core/interfaces/response/delete.interface';
+
+export class MaintenanceBaseController<T extends HRISBaseEntity> {
+  /**
+   *
+   * @param baseService
+   * @param Model
+   */
+  constructor(
+    private readonly baseService: BaseService<T>,
+    private readonly Model: typeof HRISBaseEntity,
+  ) {}
+
+  /**
+   *
+   * @param query
+   */
+  @Get()
+  @UseGuards(SessionGuard)
+  async findAll(@Query() query): Promise<ApiResult> {
+    if (query.paging === 'false') {
+      const allContents: T[] = await this.baseService.findAll();
+      return { [this.Model.plural]: allContents };
+    }
+
+    const pagerDetails: Pager = getPagerDetails(query);
+
+    const [entityRes, totalCount]: [
+      T[],
+      number,
+    ] = await this.baseService.findAndCount(
+      query.fields,
+      query.filter,
+      pagerDetails.pageSize,
+      pagerDetails.page - 1,
+    );
+
+    return {
+      pager: {
+        ...pagerDetails,
+        pageCount: entityRes.length,
+        total: totalCount,
+        nextPage: `/api/${this.Model.plural}?page=${pagerDetails.page + 1}`,
+      },
+      [this.Model.plural]: _.map(entityRes, convertUidsToIds),
+    };
+  }
+
+  /**
+   *
+   * @param req
+   * @param res
+   * @param params
+   */
+  @Get(':id')
+  @UseGuards(SessionGuard)
+  async findOne(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Param() params,
+  ): Promise<ApiResult> {
+    try {
+      const isExist = await this.baseService.findOneByUid(params.id);
+      const getResponse = isExist;
+      if (isExist !== undefined) {
+        return getSuccessResponse(res, convertUidsToIds(getResponse));
+      } else {
+        return genericFailureResponse(res, params);
+      }
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+
+  /**
+   *
+   * @param req
+   * @param res
+   * @param params
+   */
+  @Get(':id/:relation')
+  @UseGuards(SessionGuard)
+  async findOneRelation(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Param() params,
+  ): Promise<ApiResult> {
+    try {
+      const isExist = await this.baseService.findOneByUid(params.id);
+      const getResponse = isExist;
+      if (isExist !== undefined) {
+        return { [params.relation]: getResponse[params.relation] };
+      } else {
+        return genericFailureResponse(res, params);
+      }
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+
+  /**
+   *
+   * @param req
+   * @param res
+   * @param createEntityDto
+   */
+  @Post()
+  @UseGuards(SessionGuard)
+  async create(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Body() createEntityDto,
+  ): Promise<ApiResult> {
+    try {
+      const procCreateEntityDTO = await IDTOUIDObjectPropsResolver(
+        createEntityDto,
+      );
+      const isIDExist = await this.baseService.findOneByUid(
+        procCreateEntityDTO,
+      );
+      if (isIDExist !== undefined) {
+        return entityExistResponse(res, isIDExist);
+      } else {
+        const createdEntity = await this.baseService.create(
+          procCreateEntityDTO,
+        );
+        if (createdEntity !== undefined) {
+          const isPropExcluded = delete createdEntity.id;
+          return isPropExcluded
+            ? postSuccessResponse(res, createdEntity)
+            : postSuccessResponse(res, createdEntity);
+        } else {
+          return genericFailureResponse(res);
+        }
+      }
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+
+  /**
+   *
+   * @param params
+   * @param updateEntityDto
+   */
+  @Put(':id')
+  @UseGuards(SessionGuard)
+  async update(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Param() params,
+    @Body() updateEntityDto,
+  ): Promise<ApiResult> {
+    const updateEntity = await this.baseService.findOneByUid(params.id);
+    if (updateEntity !== undefined) {
+      const resolvedEntityDTO: any = await this.baseService.EntityUidResolver(
+        updateEntityDto,
+        updateEntity,
+      );
+      // ! Removed Update Based By UID params and update automatically
+      // ! By following the criteria if the uid exist the it will update
+      // ! The item but if it is new then it will create new item
+      const payload = await this.baseService.update(resolvedEntityDTO);
+      if (payload) {
+        return res
+          .status(res.statusCode)
+          .json({ message: `Item with id ${params.id} updated successfully.` });
+      }
+    } else {
+      return genericFailureResponse(res, params);
+    }
+    return null;
+  }
+
+  /**
+   *
+   * @param params
+   * @param req
+   * @param res
+   */
+  @Delete(':id')
+  @UseGuards(SessionGuard)
+  async delete(
+    @Param() params,
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Promise<ApiResult> {
+    try {
+      const isExist = await this.baseService.findOneByUid(params.id);
+      if (isExist !== undefined) {
+        const deleteResponse: DeleteResponse = await this.baseService.delete(
+          params.id,
+        );
+        return deleteSuccessResponse(req, res, params, deleteResponse);
+      } else {
+        return genericFailureResponse(res, params);
+      }
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+
+  // TODO: give descriptive name for this method
+  get plural() {
+    throw Error('Plural Not set');
+    return 'undefined';
+  }
+}
