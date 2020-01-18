383488a32a28650e842be8fe351e209331ce2672
Rajab Mkomwa <rajeyw@gmail.com>
2020-01-18 17:02:52 +0300

    fix: remove unused and deleted import

--- src/modules/system/task/services/Task.service.ts
+++ src/modules/system/task/services/Task.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { BaseService } from 'src/core/services/base.service';
 import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
+
 import { Task } from '../entities/Task.entity';
-import {Status} from '../status.models';
+
 @Injectable()
-export class TaskService extends BaseService<Task>{
-    constructor(@InjectRepository(Task)
+export class TaskService extends BaseService<Task> {
+  constructor(
+    @InjectRepository(Task)
     taskRepository: Repository<Task>,
-    ){
-        super(taskRepository, Task);
-    }
+  ) {
+    super(taskRepository, Task);
+  }
 }
