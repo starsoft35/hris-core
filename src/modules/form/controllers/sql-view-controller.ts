63f1ef6f8f6e8b2558c86d48abe2e07df22f3931
Bennett <41388684+BaharaJr@users.noreply.github.com>
2020-01-23 05:14:52 +0300

    improvement: sqlview structure the dhis way

--- src/modules/form/controllers/sql-view-controller.ts
+++ src/modules/form/controllers/sql-view-controller.ts
@@ -1,42 +1,43 @@
-import { Controller} from '@nestjs/common';
-import {
-    Get,
-    Param,
-    UseGuards,
-    Res,
-    Req,
-} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { Get, Param, UseGuards, Res, Req } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { BaseController } from 'src/core/controllers/base.contoller';
 import { SqlView } from '../entities/sqlview.entity';
 import { SqlViewService } from '../services/sql-view.service';
 import { SessionGuard } from '../../../modules/system/user/guards/session.guard';
 import { ApiResult } from 'src/core/interfaces';
-import { getSuccessResponse, genericFailureResponse } from 'src/core/helpers/response.helper';
+import {
+  getSuccessResponse,
+  genericFailureResponse,
+} from 'src/core/helpers/response.helper';
 
 @Controller('api/' + SqlView.plural)
 export class SqlViewController extends BaseController<SqlView> {
-    constructor(private service: SqlViewService) {
-        super(service, SqlView);
-    }
+  constructor(private service: SqlViewService) {
+    super(service, SqlView);
+  }
 
-    /**
+  /**
    *
    * @param req
    * @param res
    * @param params
    */
-    @Get(':id/data')
-    @UseGuards(SessionGuard)
-    async getData(
-        //@Req() req: Request,
-        @Res() res: Response,
-        @Param() params,
-    ): Promise<ApiResult> {
-        try {
-            return getSuccessResponse(res, await this.service.envokeSQL(params.id));
-        } catch (error) {
-            res.status(400).json({ error: error.message });
-        }
+  @Get(':id/data')
+  @UseGuards(SessionGuard)
+  async getData(
+    //@Req() req: Request,
+    @Res() res: Response,
+    @Param() params,
+  ): Promise<ApiResult> {
+    try {
+      let success = getSuccessResponse(
+        res,
+        await this.service.envokeSQL(params.id),
+      );
+      return success;
+    } catch (error) {
+      res.status(400).json({ error: error.message });
     }
+  }
 }
