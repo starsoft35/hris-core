db8bcac9bd40cc3d5ca652eac0be6618fd1af306
Bennett <41388684+BaharaJr@users.noreply.github.com>
2020-01-26 16:41:13 +0300

    feat: create a new cron to run a task in schedule

--- src/modules/system/schedule/controllers/schedule.controlles.ts
+++ src/modules/system/schedule/controllers/schedule.controlles.ts
@@ -1,21 +1,51 @@
-import { Controller, Res, Req, Body } from '@nestjs/common';
-import {ScheduleService} from '../services/schedule.service';
+import { Controller, Res, Req, Body, Logger } from '@nestjs/common';
+import { ScheduleService } from '../services/schedule.service';
 import { BaseController } from 'src/core/controllers/base.contoller';
 import { Schedule } from '../entities/schedule.entity';
 import { ApiResult } from 'dist/core/interfaces';
 import { Request, Response } from 'express';
+import { SchedulerRegistry, Cron } from '@nestjs/schedule';
+import {CronJob} from 'cron';
 
 @Controller('api/' + Schedule.plural)
-export class ScheduleController extends BaseController<Schedule>{
-    constructor(scheduleService: ScheduleService){
-        super(scheduleService, Schedule);
-    }
-    async create(
-        @Req() req: Request,
-        @Res() res: Response,
-        @Body() createEntityDto,
-      ): Promise<ApiResult> {
-        let results = await super.create(req,res,createEntityDto);
-        return results;
-      }
+export class ScheduleController extends BaseController<Schedule> {
+  logger: Logger;
+  scheduler: any;
+  constructor(
+    scheduleService: ScheduleService,
+    private readonly schedulerRegistry: SchedulerRegistry,
+  ) {
+    super(scheduleService, Schedule);
+  }
+  async create(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Body() createEntityDto,
+  ): Promise<ApiResult> {
+    let results = await super.create(req, res, createEntityDto);
+    return results;
+  }
+
+  @Cron('* * * * *', {
+    name: 'notifications',
+  })
+  triggerNotifications() {
+    const job = this.schedulerRegistry.getCronJob('notifications');
+
+    job.stop();
+    console.log(job.lastDate());
+  }
+  addCronJob(name: string, seconds: string) {
+    const job = new CronJob(`${seconds} * * * * *`, () => {
+      this.logger.warn(`time (${seconds}) for job ${name} to run!`);
+    });
+
+    this.scheduler.addCronJob(name, job);
+    job.start();
+
+    this.logger.warn(
+      `job ${name} added for each minute at ${seconds} seconds!`,
+    );
+  }
+
 }
