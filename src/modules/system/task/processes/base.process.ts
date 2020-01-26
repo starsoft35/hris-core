feb46e22dd2118554da920167f332f3fb6922637
Bennett <41388684+BaharaJr@users.noreply.github.com>
2020-01-27 00:43:29 +0300

    improvement(cron): call the cron to run a task in schedule

--- src/modules/system/task/processes/base.process.ts
+++ src/modules/system/task/processes/base.process.ts
@@ -1,22 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { TaskService } from '../services/task.service';
-import { Repository } from 'typeorm';
-
 import { Task } from '../entities/task.entity';
+import { ScheduleService } from '../../schedule/services/schedule.service';
+import { job } from 'cron';
 
 @Injectable()
 export class BackgroundProcess {
-  protected task:Task
-  constructor(private taskService: TaskService) {}
-  async start(task:Task){
+  protected task: Task;
+  constructor(
+    private taskService: TaskService,
+    private scheduleService: ScheduleService,
+  ) {}
+  async start(task: Task) {
     this.task = task;
     await this.run();
   }
-  async run(){
-      throw('Run Not Implemented')
+  async run() {
+    try {
+      this.scheduleService.addCronJob(name, "60");  // ! ERROR missing argument 
+    } catch {
+      throw 'Cron Not Defined';
+    }
   }
-  async log(logdetails:{type:'ERROR'|'INFO'|'SUCCESS'|'WARNING',message:string,code?:number}){
+  async log(logdetails: {
+    type: 'ERROR' | 'INFO' | 'SUCCESS' | 'WARNING';
+    message: string;
+    code?: number;
+  }) {
     this.task.log.push(logdetails);
     return await this.taskService.update(this.task);
   }
