cd1f173f69751daeaac9046581dd9c217a221b27
Bennett <41388684+BaharaJr@users.noreply.github.com>
2020-01-27 00:43:04 +0300

    improvement(cron): define cron to run a task in schedule

--- src/modules/system/schedule/services/schedule.service.ts
+++ src/modules/system/schedule/services/schedule.service.ts
@@ -1,19 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { BaseService } from 'src/core/services/base.service';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Schedule } from '../entities/schedule.entity';
+import { SchedulerRegistry, Cron } from '@nestjs/schedule';
+import { CronJob } from 'cron';
 
 @Injectable()
-export class ScheduleService extends BaseService<Schedule>{
-    constructor(@InjectRepository(Schedule)
+export class ScheduleService extends BaseService<Schedule> {
+  logger: Logger;
+  scheduler: any;
+  constructor(
+    @InjectRepository(Schedule)
     scheduleRepository: Repository<Schedule>,
-    ){
-        super(scheduleRepository, Schedule);
-    }
+    private readonly schedulerRegistry: SchedulerRegistry,
+  ) {
+    super(scheduleRepository, Schedule);
+  }
 
-    async getProcess(){
-        return this.findOneByUid
-    }
+  async getProcess() {
+    return this.findOneByUid;
+  }
+  @Cron('* * * * *', {
+    name: 'dhis 2 hrhis sync',
+  })
+  triggerNotifications() {
+    const job = this.schedulerRegistry.getCronJob('dhis 2 hrhis sync');
+
+    job.stop();
+    console.log(job.lastDate());
+  }
+  addCronJob(name: string, seconds: string) {
+    const job = new CronJob(`${seconds} * * * * *`, () => {
+      this.logger.warn(`time (${seconds}) for job ${name} to run!`);
+    });
+
+    this.scheduler.addCronJob(name, job);
+    job.start();
+
+    this.logger.warn(
+      `${name} will sync every ${seconds} seconds!`,
+    );
+  }
 }
-
