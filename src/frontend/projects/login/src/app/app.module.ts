17b63244d92a3352f2fbea08d49e7b2b65affec4
Vincent Minde <vincentminde@gmail.com>
2020-01-27 19:05:16 +0300

    Added login code

--- src/frontend/projects/login/src/app/app.module.ts
+++ src/frontend/projects/login/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './store/reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { pages } from './pages';
+import { effects } from './store/effects';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ...pages
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    SharedModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot(effects)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/frontend/projects/login/src/app/shared/services/index.ts
@@ -0,0 +1,6 @@
+import {HttpClientService} from './http-client.service';
+import {LoginService} from './login.service';
+
+export const services: any[] = [
+  HttpClientService, LoginService
+];
