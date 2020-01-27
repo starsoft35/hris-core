629c6f7d21100e4b114399a5a5687c6997b558e7
Vincent Minde <vincentminde@gmail.com>
2020-01-27 09:08:31 +0300

    Removed unneccessary consoles

--- src/core/helpers/user-decorator.helper.ts
+++ src/core/helpers/user-decorator.helper.ts
@@ -1,6 +1,5 @@
 import { createParamDecorator } from "@nestjs/common";
 
 export const AuthenticatedUser = createParamDecorator((data, req) => {
-    console.log('What:',req.session)
     return req.session.user;
 });
