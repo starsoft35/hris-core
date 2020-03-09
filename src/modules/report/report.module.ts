e454f8ffec3ff83926e3db457bc112609e932694
Bennett <bennybenester@gmail.com>
2020-03-09 14:57:06 +0300

    feat: add report group entity to imports in report module

--- src/modules/report/report.module.ts
+++ src/modules/report/report.module.ts
@@ -6,11 +6,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Report } from './entities/report.entity';
 import { ReportGroupController } from './controllers/reportgroup.controller';
 import { ReportGroupService } from './services/reportgroup.service';
+import { ReportGroup } from './entities/report.group.entity';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'basic', session: true }),
-    TypeOrmModule.forFeature([Report]),
+    TypeOrmModule.forFeature([Report, ReportGroup]),
   ],
   controllers: [ReportController, ReportGroupController],
   providers: [ReportService, ReportGroupService],
