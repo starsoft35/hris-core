d11356bf1be0d70a69960fe5872bd6f5161392bd
Bennett <bennybenester@gmail.com>
2020-03-09 14:38:33 +0300

    feat: implement reportgroup endpoints

--- src/modules/report/report.module.ts
+++ src/modules/report/report.module.ts
@@ -4,13 +4,15 @@ import { ReportService } from './services/report.service';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Report } from './entities/report.entity';
+import { ReportGroupController } from './controllers/reportgroup.controller';
+import { ReportGroupService } from './services/reportgroup.service';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'basic', session: true }),
     TypeOrmModule.forFeature([Report]),
   ],
-  controllers: [ReportController],
-  providers: [ReportService],
+  controllers: [ReportController, ReportGroupController],
+  providers: [ReportService, ReportGroupService],
 })
 export class ReportModule {}
