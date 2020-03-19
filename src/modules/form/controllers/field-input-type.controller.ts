0963b61dc9c9fe59fcdb9b219a74d0837471535d
Bennett <bennybenester@gmail.com>
2020-03-20 02:23:20 +0300

    fix: fix an issue with failing to get one fieldinput type

--- src/modules/form/controllers/field-input-type.controller.ts
+++ src/modules/form/controllers/field-input-type.controller.ts
@@ -1,10 +1,10 @@
 import { Controller } from '@nestjs/common';
 import { FieldInputType } from '../entities/field-input-type.entity';
 import { FieldInputTypeService } from '../services/field-input-type.service';
-import { MaintenanceBaseController } from 'src/core/maintenance/controllers/base.contoller';
+import { BaseController } from 'src/core/controllers/base.contoller';
 
 @Controller('api/' + FieldInputType.plural)
-export class FieldInputTypeController extends MaintenanceBaseController<
+export class FieldInputTypeController extends BaseController<
   FieldInputType
 > {
   constructor(fieldInputTypeService: FieldInputTypeService) {
