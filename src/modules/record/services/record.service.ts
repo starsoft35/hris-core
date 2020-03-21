a51a70f6ec32c5f33c556f5e6f0dc3f0c8a49730
Bennett <bennybenester@gmail.com>
2020-03-21 19:15:21 +0300

    feat: clean up fetching one record

--- src/modules/record/services/record.service.ts
+++ src/modules/record/services/record.service.ts
@@ -236,20 +236,4 @@ export class RecordService extends BaseService<Record> {
     console.log('Fetching By UID');
     return await this.recordRepository.findOne({ where: { uid }, join });
   }
-
-  /**
-   *
-   * @param id
-   */
-  async findOneById(id: string): Promise<Record> {
-    let join: any = {};
-    join = {
-      alias: 'record',
-      leftJoinAndSelect: {
-        form: 'record.form',
-        organisationUnit: 'record.organisationUnit',
-      },
-    };
-    return await this.recordRepository.findOne({ where: { id }, join });
-  }
 }
