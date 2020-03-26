03d84c7930ec6e8e142cc365a214672103500372
Bennett <bennybenester@gmail.com>
2020-03-26 12:06:28 +0300

    feat: add sqlview endpoint to report module

--- src/modules/report/controllers/report.controller.ts
+++ src/modules/report/controllers/report.controller.ts
@@ -1,11 +1,37 @@
-import { Controller } from '@nestjs/common';
-import {ReportService} from '../services/report.service';
+import { Get, Param, UseGuards, Res, Req, Controller } from '@nestjs/common';
+import { Request, Response } from 'express';import { ReportService } from '../services/report.service';
 import { BaseController } from 'src/core/controllers/base.contoller';
 import { Report } from '../entities/report.entity';
+import { SessionGuard } from '../../../modules/system/user/guards/session.guard';
+import { ApiResult } from 'src/core/interfaces';
+import {
+  getSuccessResponse,
+  genericFailureResponse,
+} from 'src/core/helpers/response.helper';
 
 @Controller('api/' + Report.plural)
-export class ReportController extends BaseController<Report>{
-    constructor(ReportService: ReportService){
-        super(ReportService, Report);
+export class ReportController extends BaseController<Report> {
+  constructor(private ReportService: ReportService) {
+    super(ReportService, Report);
+  }
+
+  /**
+   *
+   * @param req
+   * @param res
+   * @param params
+   */
+  @Get('/sqlViews/:id/data')
+  @UseGuards(SessionGuard)
+  async getData(
+    //@Req() req: Request,
+    @Res() res: Response,
+    @Param() params,
+  ): Promise<ApiResult> {
+    try {
+      return getSuccessResponse(res, await this.ReportService.envokeSQL(params.id));
+    } catch (error) {
+      res.status(400).json({ error: error.message });
     }
+  }
 }
--- src/modules/report/services/report.service.ts
+++ src/modules/report/services/report.service.ts
@@ -1,14 +1,63 @@
 import { Injectable } from '@nestjs/common';
 import { BaseService } from 'src/core/services/base.service';
 import { Report } from '../entities/report.entity';
-import { Repository } from 'typeorm';
+import { Repository, Connection } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class ReportService extends BaseService<Report>{
-    constructor(@InjectRepository(Report)
+export class ReportService extends BaseService<Report> {
+  constructor(
+    @InjectRepository(Report)
     reportRepository: Repository<Report>,
-    ){
-        super(reportRepository, Report);
+    private readonly connection: Connection,
+  ) {
+    super(reportRepository, Report);
+  }
+
+  async envokeSQL(id) {
+    const sqlView = await this.findOneByUid(id);
+    let results = {
+      pager: {
+        page: 1,
+        pageCount: 285,
+        total: 14220,
+        pageSize: 50,
+      },
+      listGrid: {
+        metaData: {},
+        headerWidth: 0,
+        subtitle: '',
+        width: 0,
+        title: 'all_account_details',
+        height: 0,
+        headers: [],
+        rows: [],
+      },
+    };
+    let data = await this.connection.query(sqlView.code);
+    console.log('Data:', data[0]);
+    if (data.length > 0) {
+      results.listGrid.headers = Object.keys(data[0]).map(key => {
+        return {
+          hidden: false,
+          meta: false,
+          name: key,
+          column: key,
+          type: 'string',
+        };
+      });
+      results.listGrid.rows = data.map(row => {
+        var newRow = [];
+        results.listGrid.headers.forEach((header, index) => {
+          newRow[index] = row[header.name];
+        });
+        return newRow;
+      });
+      results.listGrid.height = results.listGrid.rows.length;
+      results.listGrid.headerWidth = results.listGrid.headers.length;
+      results.listGrid.width = results.listGrid.headers.length;
     }
+    results.listGrid.title = sqlView.name;
+    return results;
+  }
 }
