f67a5ac21732e1bfd60f88ad4ba2d1f22a0dd002
Bennett <bennybenester@gmail.com>
2020-04-02 23:49:56 +0300

    refactor: add entities for participants and facilitators

--- src/modules/training/entities/training-session-facilitatory.entity.ts
+++ src/modules/training/entities/training-session-facilitatory.entity.ts
@@ -1,16 +1,43 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-import { TransactionTimestamp } from 'src/core/entities/transaction-timestamp.entity';
+import { Column, Entity } from 'typeorm';
 
 @Entity('sessionfacilitator', { schema: 'public' })
-export class SessionFacilitator extends TransactionTimestamp {
-  static plural = 'facilitators';
-
-  @PrimaryGeneratedColumn()
+export class SessionFacilitator {
+  @Column('integer', {
+    nullable: false,
+    primary: true,
+    name: 'id',
+  })
   id: number;
 
-  @Column()
+  @Column('character varying', {
+    nullable: false,
+    length: 13,
+    name: 'uid',
+  })
+  uid: string;
+
+  @Column('integer', {
+    nullable: false,
+    name: 'sessionid',
+  })
   sessionid: number;
 
-  @Column()
+  @Column('integer', {
+    nullable: false,
+    name: 'recordid',
+  })
   recordid: number;
+
+  @Column('timestamp without time zone', {
+    nullable: false,
+    name: 'datecreated',
+  })
+  datecreated: Date;
+
+  @Column('timestamp without time zone', {
+    nullable: true,
+    default: () => 'NULL::timestamp without time zone',
+    name: 'lastupdated',
+  })
+  lastupdated: Date | null;
 }
--- src/modules/training/entities/training-session-participant.entity.ts
+++ src/modules/training/entities/training-session-participant.entity.ts
@@ -1,16 +1,30 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-import { TransactionTimestamp } from 'src/core/entities/transaction-timestamp.entity';
+import { Column, Entity } from 'typeorm';
 
 @Entity('sessionparticipant', { schema: 'public' })
-export class SessionParticipant extends TransactionTimestamp {
-  static plural = 'participants';
-
-  @PrimaryGeneratedColumn()
+export class SessionParticipant {
+  @Column('integer', {
+    nullable: false,
+    primary: true,
+    name: 'id',
+  })
   id: number;
 
-  @Column()
+  @Column('character varying', {
+    nullable: false,
+    length: 13,
+    name: 'uid',
+  })
+  uid: string;
+
+  @Column('integer', {
+    nullable: false,
+    name: 'sessionid',
+  })
   sessionid: number;
 
-  @Column()
+  @Column('integer', {
+    nullable: false,
+    name: 'recordid',
+  })
   recordid: number;
 }
