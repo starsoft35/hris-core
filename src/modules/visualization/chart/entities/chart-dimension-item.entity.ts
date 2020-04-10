50835f856b230a0276e4ac6ca6c18ed9063f8f5d
Bennett <bennybenester@gmail.com>
2020-04-10 18:11:37 +0300

    feat: add missing columns for chart dimension entity

--- src/modules/visualization/chart/entities/chart-dimension-item.entity.ts
+++ src/modules/visualization/chart/entities/chart-dimension-item.entity.ts
@@ -1,9 +1,17 @@
-import { Entity, ManyToOne, JoinColumn } from 'typeorm';
+import { Entity, ManyToOne, JoinColumn, PrimaryGeneratedColumn, Column } from 'typeorm';
 import { ChartDimension } from './chart-dimension.entity';
 import { FavoriteDimensionItem } from '../../dashboard/entities/favorite-dimension-item.entity';
 
 @Entity('chartdimensionitem', { schema: 'public' })
 export class ChartDimensionItem extends FavoriteDimensionItem {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  uid: string;
+
+  @Column()
+  displayname: string;
   @ManyToOne(
     () => ChartDimension,
     (chartDimension: ChartDimension) => chartDimension.items,
