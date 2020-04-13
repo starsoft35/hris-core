3eef70d2d178bc7467ee0688b58a091b0148b9eb
Bennett <bennybenester@gmail.com>
2020-04-13 10:38:57 +0300

    feat: add get participants endpoint

--- src/modules/training/controllers/training-session.controller.ts
+++ src/modules/training/controllers/training-session.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards, Param, HttpStatus, Res } from '@nestjs/common';
 import { BaseController } from '../../../core/controllers/base.contoller';
 
 import { TrainingSessionService } from '../services/training-session.service';
 import { TrainingSession } from '../entities/training-session.entity';
 import { SessionGuard } from 'src/modules/system/user/guards/session.guard';
+import { sanitizeResponseObject } from 'src/core/utilities/sanitize-response-object';
 
 @Controller('api/training/' + TrainingSession.plural)
 export class TrainingSessionController extends BaseController<
@@ -17,4 +18,15 @@ TrainingSession
     async delivery (){
       return this.trainingSessionService.deliverymode()
     }
+    @Get(':uid/participants')
+    @UseGuards(SessionGuard)
+    async getParticipants(@Param() uid, @Res() res
+    ){
+      console.log('sessions', uid)
+      const sessions = await this.trainingSessionService.getParticipants(uid)
+      return res
+      .status(HttpStatus.OK)
+      .send(sanitizeResponseObject(sessions));
   }
+
+    }
