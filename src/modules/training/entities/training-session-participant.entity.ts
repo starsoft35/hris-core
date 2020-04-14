b0850a16d6741bdf5b339731a6037a8854533b1a
Bennett <bennybenester@gmail.com>
2020-04-14 11:58:45 +0300

    feat: add uid and id to participants entity

--- src/modules/training/entities/training-session-participant.entity.ts
+++ src/modules/training/entities/training-session-participant.entity.ts
@@ -1,29 +1,29 @@
 import { Column, Entity, OneToMany, ManyToMany, ManyToOne } from 'typeorm';
-import { Record } from 'src/modules/record/entities/record.entity';
+import { Record } from '../../../modules/record/entities/record.entity';
 import { TrainingSession } from './training-session.entity';
 
 @Entity('sessionparticipant', { schema: 'public' })
 export class SessionParticipant {
-  // @Column('integer', {
-  //   nullable: false,
-  //   primary: true,
-  //   name: 'id',
-  // })
-  // id: number;
+  @Column('integer', {
+    nullable: false,
+    primary: true,
+    name: 'id',
+  })
+  id: number;
 
-  // @Column('character varying', {
-  //   nullable: false,
-  //   length: 13,
-  //   name: 'uid',
-  // })
-  // uid: string;
+  @Column('character varying', {
+    nullable: false,
+    length: 13,
+    name: 'uid',
+  })
+  uid: string;
 
   @Column('integer', {
     nullable: false,
     name: 'trainingsessionId',
     primary: true,
   })
-  trainingSessionId: number;
+  trainingsessionId: number;
 
   @Column('integer', {
     nullable: false,
@@ -43,7 +43,7 @@ export class SessionParticipant {
     record => record.participants,
     { eager: true },
   )
-  record: Record[];
+  participants: Record[];
   static plural: any = 'participants';
 
   /*@ManyToOne(
